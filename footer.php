315c99dc355fd8ecac0a2ffb2bc2e614669649d9
mariussunde <mariussunde@snohetta.com>
2017-05-29 11:18:59 +0200

    Updated filestructure

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 <footer class="footer-main grid"></footer>
 
 </div><!-- .site -->
-<script src="js/jquery-3.2.0.min.js"></script>
-<script src="js/velocity.min.js"></script>
-<script src="js/jquery.smoothState.min.js"></script>
-<script src="js/site.js"></script>
+<script src="assets/js/jquery-3.2.0.min.js"></script>
+<script src="assets/js/velocity.min.js"></script>
+<script src="assets/js/jquery.smoothState.min.js"></script>
+<script src="assets/js/site.js"></script>
 </body>
 </html>
