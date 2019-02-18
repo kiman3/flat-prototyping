47cee825ba292189ba50586297dd377b82143b66
Kim Andre Fosslien Ottesen <kimandreottesen@SNO-KimAmbp.local>
2019-02-18 17:25:55 +0100

    Less opinionated, removed grid from default footer template, updated Flexboxgrid, fixed footer-main class

--- footer.php
+++ footer.php
@@ -1,30 +1,10 @@
-	<footer class="footer-main grid"></footer>
-	
-	<!-- Grid widget. Toggle by pressing "g" -->
-	<div class="grid-widget">
-		<div class="row">
-			<div class="col-xs-1"><span>1</span></div>
-			<div class="col-xs-1"><span>2</span></div>
-			<div class="col-xs-1"><span>3</span></div>
-			<div class="col-xs-1"><span>4</span></div>
-			<div class="col-xs-1"><span>5</span></div>
-			<div class="col-xs-1"><span>6</span></div>
-			<div class="col-xs-1"><span>7</span></div>
-			<div class="col-xs-1"><span>8</span></div>
-			<div class="col-xs-1"><span>9</span></div>
-			<div class="col-xs-1"><span>10</span></div>
-			<div class="col-xs-1"><span>11</span></div>
-			<div class="col-xs-1"><span>12</span></div>
-		</div>
-	</div>
+	<footer class="footer-main row"></footer>
 
 </div><!-- .site -->
 <script>
 	document.body.className = document.body.className.replace("no-js","js");
 </script>
 <script src="assets/js/jquery.min.js"></script>
-<script src="assets/js/velocity.min.js"></script>
-<script src="assets/js/jquery.smoothState.min.js"></script>
 <script src="assets/js/site.js"></script>
 </body>
 </html>
